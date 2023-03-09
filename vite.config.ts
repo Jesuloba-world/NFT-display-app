29fc51b74efa11dfef7e55184d59d074ed63ae40
jesuloba-world <jesulobajohn@gmail.com>
2023-03-09 14:43:08 +0100

    test this

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), viteTsconfigPaths(), svgr({ exportAsDefault: true })],
-	define: {
-		global: {},
-	},
+	// define: {
+	// 	global: {},
+	// },
 });
